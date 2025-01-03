4339066233611e710a7e9a9a453ca1f3b8858889
holausman <126561345+holausman@users.noreply.github.com>
2025-01-03 16:22:32 +0300

    homepage design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,40 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def global_corporate_strategy
+    # Global Corporate Strategy page action
+  end
+
+  def intellectual_property
+    # Intellectual Property page action
+  end
+
+  def finance_and_investments
+    # Finance & Investments page action
+  end
+
+  def legal_and_compliance
+    # Legal & Compliance page action
+  end
+
+  def operational_excellence
+    # Operational Excellence page action
+  end
+
+  def risk_esg_and_governance
+    # Risk, ESG & Governance page action
+  end
+
+  def single_north_star
+    # Single North Star principle page action
+  end
+
+  def live_impact_thrive
+    # Live, Impact & Thrive principle page action
+  end
+
+  def impact_beyond_us
+    # Impact Beyond Us principle page action
+  end
 end
